656269bc7dfb47ae24f810f53de2d0b9fadff5f3
2005346HemaPrakash <66317037+2005346HemaPrakash@users.noreply.github.com>
2020-06-07 16:30:27 +0530

    Update democ.c

--- democ.c
+++ democ.c
@@ -2,10 +2,11 @@
 #include "stdlib.h"
 
 int main(){
-  int x;
+  
   char name[5];
+  
   printf("Enter the name:\n");
-  scanf("%s",name);
-  printf("%s\n",name);
+  scanf("%4s",name);
+  printf("%4s\n",name);
   return 0;
 }
